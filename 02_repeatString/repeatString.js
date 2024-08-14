827f3320fdce99b71ad2474cb5da0d7048b5448d
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-08-13 21:47:07 -0400

    Modified repeatString.js / All tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    let string = ""; 
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < times; i++) {
+        string += word; 
+    }
+    return string; 
+}
 
-};
+//Always check the tests first since the function in the main .js file will work for all the tests provided. 
 
 // Do not edit below this line
 module.exports = repeatString;
