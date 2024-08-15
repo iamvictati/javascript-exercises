837137676dfbb97f0d7dd44a8c9ddfb61e2653c9
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-08-14 20:16:42 -0400

    Modified reverseString.js / All tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let joinPhrase = ""; 
+    splitPhrase = phrase.split("");
+    reversePhrase = splitPhrase.reverse(); 
+    joinPhrase = reversePhrase.join(""); 
+    return joinPhrase; 
 };
 
 // Do not edit below this line
