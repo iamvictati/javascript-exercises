e3736cecc8781fd84b0d029064a8a002b2a259c7
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-08-15 20:38:17 -0400

    Modified removeFromArray.spec.js / Solution checked to pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //SOLUTION: USE ARROW FUNCTION to FILTER ANYTHING IN array EXCEPT args ELEMENTS. 
+    //return array.filter(element => !args.includes(val))
+
+    //Using a named function: 
+    function anythingButArgs(element) {
+        return !args.includes(element); 
+    }
+    return array.filter(anythingButArgs); 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//NOTES: 
+// First, ask yourself if a loop is actually necessary at all. 
+// Second, if not, then a simple new value assignment operation will do the trick. 
