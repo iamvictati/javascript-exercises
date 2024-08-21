cefa4277327973ef6833ab007de258e08ca5f0ce
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-08-21 19:46:37 -0400

    Carl Gauss's formula // 2 tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,11 @@
 const sumAll = function(firstNum, secondNum) {
+    // use CARL CAUSS'S FORMULA 
+    // n = lastNum - firstNum + 1; 
+    // sum of consecutive numbers = (n/2) * (firstNum + lastNum) 
     
     for (let i = 0; i <= secondNum; i++) {
-        result = firstNum + firstNum++; 
+        n = secondNum - firstNum + 1; 
+        result = (n/2) * (firstNum + secondNum); 
     }
     return result; 
 
