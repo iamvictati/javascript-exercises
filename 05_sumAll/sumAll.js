a5bbccfcd8bbb4250a72766d235413405073d3bb
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-08-22 17:56:19 -0400

    All tests passed // Solution: long & inefficient

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -8,7 +8,8 @@ const sumAll = function(firstNum, secondNum) {
     } else if ((firstNum - Math.floor(firstNum)) !== 0
         || (secondNum - Math.floor(secondNum)) !== 0) {
         return "ERROR";
-    } else if (isNaN(firstNum) || isNaN(secondNum)) {
+    } else if (typeof firstNum !== "number" 
+        || typeof secondNum !== "number") {
         return "ERROR"; 
     }
 
@@ -23,13 +24,25 @@ const sumAll = function(firstNum, secondNum) {
     }
     return result; 
 
+    //A MUCH BETTER SOLUTION: 
+
+    /*  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+        if (min < 0 || max < 0) return "ERROR";
+        if (min > max) {
+            const temp = min;
+            min = max;
+            max = temp;
+        }
+        
+        let sum = 0;
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    */
 };
 
 // Do not edit below this line
 module.exports = sumAll;
 
-/*  for (let i = 0; i <= secondNum; i++) {
-        n = secondNum - firstNum + 1; 
-        result = (n/2) * (firstNum + secondNum); 
-    }
-    return result; */
+
