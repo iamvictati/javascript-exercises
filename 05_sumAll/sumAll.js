0ba6de1ee33defc12f8a511cc7822cd2a5fdd5b5
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-08-22 17:37:50 -0400

    Modified sumAll.js // 5 tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,10 +2,24 @@ const sumAll = function(firstNum, secondNum) {
     // use CARL CAUSS'S FORMULA 
     // n = lastNum - firstNum + 1; 
     // sum of consecutive numbers = (n/2) * (firstNum + lastNum) 
-    
-    for (let i = 0; i <= secondNum; i++) {
-        n = secondNum - firstNum + 1; 
-        result = (n/2) * (firstNum + secondNum); 
+
+    if (firstNum < 0|| secondNum < 0) {
+        return "ERROR"; 
+    } else if ((firstNum - Math.floor(firstNum)) !== 0
+        || (secondNum - Math.floor(secondNum)) !== 0) {
+        return "ERROR";
+    } else if (isNaN(firstNum) || isNaN(secondNum)) {
+        return "ERROR"; 
+    }
+
+    for (let i = 0; i <= Math.max(firstNum, secondNum); i++) {
+        if (secondNum > firstNum) {
+            n = secondNum - firstNum + 1; 
+            result = (n/2) * (firstNum + secondNum);
+        } else {
+            n = firstNum - secondNum + 1; 
+            result = (n/2) * (firstNum + secondNum);
+        }
     }
     return result; 
 
@@ -13,3 +27,9 @@ const sumAll = function(firstNum, secondNum) {
 
 // Do not edit below this line
 module.exports = sumAll;
+
+/*  for (let i = 0; i <= secondNum; i++) {
+        n = secondNum - firstNum + 1; 
+        result = (n/2) * (firstNum + secondNum); 
+    }
+    return result; */
