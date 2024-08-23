1ec7313c6e2c5b07edc192f8e3f9091c1047639b
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-08-22 20:58:11 -0400

    Modified leapYears.js // only 3 tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if ((year % 100 == 0 && Number.isInteger(year/100)) 
+        && !(year % 400 == 0 && Number.isInteger(year/400)) 
+        && !(year % 4 == 0 && Number.isInteger(year/4))) 
+        return false; 
+    if ((year % 4 == 0 && Number.isInteger(year/4)) 
+        || (year % 400 == 0 && Number.isInteger(year/400)))
+        return true; 
 
 };
 
