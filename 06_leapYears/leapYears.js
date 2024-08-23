e7a128b34569ef95937e22bde27bcc579e0d5880
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-08-22 21:26:02 -0400

    Solved leapYears.js // Solution: efficient

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,11 @@
 const leapYears = function(year) {
-    if ((year % 100 == 0 && Number.isInteger(year/100)) 
-        && !(year % 400 == 0 && Number.isInteger(year/400)) 
-        && !(year % 4 == 0 && Number.isInteger(year/4))) 
-        return false; 
-    if ((year % 4 == 0 && Number.isInteger(year/4)) 
-        || (year % 400 == 0 && Number.isInteger(year/400)))
-        return true; 
-
+    if (year % 4 === 0 
+        && (!(year % 100 === 0) || year % 400 === 0)) {
+    return true
+    } else {
+        return false
+    }
+// BE CAREFUL WITH THE PLACEMENT OF INTERROGATION MARKS. 
 };
 
 // Do not edit below this line
