5923e78f6c3216201ecf86b7cd9dc18609d75293
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-08-26 13:29:22 -0400

    Simplified tempConversion.js / All tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,11 @@
 const convertToCelsius = function(Fahren) { 
   const inCelsius = (Fahren - 32) * 5/9;
-  const toCelsius = Math.round(inCelsius * 10) / 10;
-  return toCelsius; 
+  return Math.round(inCelsius * 10) / 10; 
 };
 
 const convertToFahrenheit = function(Cels) {
   const inFahrenheit = (Cels * 9/5 + 32); 
-  const toFahrenheit = Math.round(inFahrenheit * 10) / 10; 
-  return toFahrenheit; 
+  return Math.round(inFahrenheit * 10) / 10;  
 };
 
 // Do not edit below this line
